630f2235d43dbde350ecf5147143fb7244d6da83
Isaac <isaacgatgens13@gmail.com>
2023-05-15 17:54:54 -0600

    Ruta medio buena

--- angular-15-client/src/app/_services/auth.service.ts
+++ angular-15-client/src/app/_services/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-const AUTH_API = 'http://138.2.231.71:8080/api/auth/';
+const AUTH_API = 'http://138.2.231.71/api/auth/';
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
